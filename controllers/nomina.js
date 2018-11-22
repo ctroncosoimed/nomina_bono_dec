e9eefc85480567538b357d953015302556fe8286
Claudio Troncoso <ctroncoso@i-med.cl>
2018-11-22 17:21:59 -0300

    Se cambio que el limit y offset fuera recibido cuando se envia por el body del request

--- controllers/nomina.js
+++ controllers/nomina.js
@@ -12,18 +12,18 @@ module.exports = {
     if (req.body.bonificacion != null && req.body.bonificacion != "") { where["bonificacion"] = req.body.bonificacion }
     if (req.body.comuna_valida != null && req.body.comuna_valida != "") { where["comuna_valida"] = req.body.comuna_valida }
     if (req.body.fecha_acto_venta != null && req.body.fecha_acto_venta != "") { where["fecha_acto_venta"] = req.body.fecha_acto_venta }
-    if (req.body.estado != null && req.body.estado != "") { where["anio"] = req.body.estado }
-    if (req.body.financiador != null && req.body.financiador != "") { where["anio"] = req.body.financiador }
+    if (req.body.estado != null && req.body.estado != "") { where["estado"] = req.body.estado }
+    if (req.body.financiador != null && req.body.financiador != "") { where["financiador"] = req.body.financiador }
     if (req.body.mes != null && req.body.mes != "") { where = sequelize.literal("EXTRACT(month FROM fecha_acto_venta) = "+req.body.mes)} 
 
-    if ( req.query.limit == null) { req.query.limit = 3 }
-    if ( req.query.offset == null) { req.query.offset = 1 }
+    if ( req.body.limit == null) { req.body.limit = 3 }
+    if ( req.body.offset == null) { req.body.offset = 1 }
 
     return nomina
     .findAndCountAll({
       where,
-      limit: req.query.limit,
-      offset: req.query.offset,
+      limit: req.body.limit,
+      offset: req.body.offset,
     })
     .then(nomina => {
      if(nomina.count <= 0){
